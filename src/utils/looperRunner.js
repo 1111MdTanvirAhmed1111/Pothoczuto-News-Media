146cafbe21dd6a3bd2c9d9efdde9883642af8572
Tanvir <freelancertanvir91@gmail.com>
2025-03-17 18:22:32 +0600

    Update URLs in runLooper function for correct endpoints

--- src/utils/looperRunner.js
+++ src/utils/looperRunner.js
@@ -2,8 +2,8 @@ const https = require('https');
 
 const runLooper = () => {
   setInterval(() => {
-    https.get("https://pothoczuto-backend.onrender.com/");
-    https.get("https://gazi-tanvir-portfolio.onrender.com/");
+    https.get("https://pothoczuto-news-media.onrender.com");
+    https.get("https://gazi-tanvir-portfolio.onrender.com/blogs");
   }, 45 * 1000);
 };
 
