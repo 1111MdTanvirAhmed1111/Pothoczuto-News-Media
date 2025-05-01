ec1e806dfb9223161ad2b252b5bd0b9ce2b8ad90
Tanvir <freelancertanvir91@gmail.com>
2025-05-01 18:07:17 +0600

    feat(routes): add getAllCategories route to retrieve all categories

--- src/controllers/CategoryControllers.js
+++ src/controllers/CategoryControllers.js
@@ -59,7 +59,18 @@ const deleteCategory = async (req, res) => {
   }
 };
 
+const getAllCategories  = async (req, res) => {
+  try {
+    const categories = await prisma.category.findMany();
+    return res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+}
+
 module.exports = {
+  getAllCategories,
   createCategory,
   deleteCategory,
 };
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,10 +1,10 @@
 const router  = require('express').Router();
-const { createCategory, deleteCategory } = require('@/controllers/CategoryControllers');
+const { createCategory, deleteCategory, getAllCategories } = require('@/controllers/CategoryControllers');
 const authMiddleware = require('@/middlewares/authMiddleware');
 const roleMiddleware = require('@/middlewares/roleMiddleware');
 
 
-router.get('/catergory')
+router.get('/',getAllCategories)
 router.post('/catergory/:id',authMiddleware ,roleMiddleware('admin'), createCategory)
 router.delete('/catergory/:id', authMiddleware ,roleMiddleware('admin'), deleteCategory)
 
